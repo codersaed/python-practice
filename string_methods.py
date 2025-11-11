99f3ac1bcc717bc92d593b7566b40e2252e4105a
codersaed <codersaed@gmail.com>
2025-11-11 17:26:55 +0600

    string_methods

---
+++ string_methods.py
@@ -0,0 +1,13 @@
+# Convert to uppercase and lowercase
+text = "learn PytHon"
+print(text.upper())
+print(text.lower())
+
+# Count how many times a word appears
+sent = "hello there, hello world!"
+print(sent.count("hello"))
+
+# Check if a string starts or ends with a word
+name = "abu saed"
+print(name.startswith("a"))
+print(name.endswith("saed"))
