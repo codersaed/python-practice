f699d7281cf6c0c853ed3300be8254db8726a1a2
codersaed <codersaed@gmail.com>
2025-11-11 17:49:15 +0600

    list_tuple

---
+++ list_tuple_dict_set.py
@@ -0,0 +1,15 @@
+#  Access items from list and tuple
+num = [10, 20,30]
+letters = ("a", "b", "c")
+print(num[0])
+print(letters[2])
+
+# Add and remove items from a dictionary
+num = [10,20,30]
+del num[2]
+print(num)
+
+# Convert list to set to remove duplicates
+nums = [1,1,2,3,3,4,5,5]
+unique = set(nums)
+print(unique)
